53355741132f78d15c71beb3c3dd1bb467809194
eunji9128 <eunji9128@gmail.com>
2025-03-26 13:16:23 +0900

    fix: hosting url typo

--- sdk.js
+++ sdk.js
@@ -18,7 +18,7 @@
             var s = document.createElement("script"); 
             s.type = "text/javascript"; 
             s.async = true; 
-            s.src = "https://d3qrvyizob9ouf.cloudfront.net/floating-button-sdk.js"; 
+            s.src = "https://sdk-gentooai.com/floating-button-sdk.js"; 
             // s.src = 'https://d32xcphivq9687.cloudfront.net/floating-button-sdk.js'; // dev
             // s.src = "./floating-button-sdk.js"; 
             s.onload = () => { 
