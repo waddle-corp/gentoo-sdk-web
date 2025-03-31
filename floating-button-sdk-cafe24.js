f6385323bf682c1f34d7d0b55d83466bd8b7b550
eunji9128 <eunji9128@gmail.com>
2025-03-31 14:05:56 +0900

    fix: url rule edited

--- floating-button-sdk-cafe24.js
+++ floating-button-sdk-cafe24.js
@@ -838,8 +838,8 @@ class FloatingButton {
      * @returns {string|null} - 추출된 product_no 값 또는 null (찾을 수 없을 경우)
      */
     getProductNo(urlString = window.location.href) {
-        if (urlString.includes('/product')) { this.displayLocation = 'PRODUCT_DETAIL' }
-        else if (urlString.includes('/category')) { this.displayLocation = 'PRODUCT_LIST' }
+        if (urlString.includes('/category') || urlString.includes('/product/list')) { this.displayLocation = 'PRODUCT_LIST' }
+        else if (urlString.includes('/product')) { this.displayLocation = 'PRODUCT_DETAIL' }
         else { this.displayLocation = 'HOME' }
         try {
             // URL 객체 생성
