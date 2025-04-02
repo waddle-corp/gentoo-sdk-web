5bf63080f6d06319ba7cd196176af8fd991527bd
eunji9128 <eunji9128@gmail.com>
2025-04-02 14:18:32 +0900

    test set 300ms

--- floating-button-sdk-cafe24.js
+++ floating-button-sdk-cafe24.js
@@ -417,7 +417,7 @@ class FloatingButton {
                 this.openChat();
                 this.gentooSessionData.redirectState = false;
                 sessionStorage.setItem('gentoo', JSON.stringify(this.gentooSessionData));
-            }, 200);
+            }, 300);
         }
         window.__GentooInited = 'created';
     }
