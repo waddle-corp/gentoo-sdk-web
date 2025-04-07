e97b2a2295319d0e786069b3289f7727261e70db
eunji9128 <eunji9128@gmail.com>
2025-04-07 22:21:31 +0900

    fix: hotfix for typo

--- floating-button-sdk-cafe24.js
+++ floating-button-sdk-cafe24.js
@@ -366,7 +366,7 @@ class FloatingButton {
                     }
                 }
                 this.openChat(e, this.elems);
-                if (this.eventCallback.click !== null) {
+                if (this.eventCallback?.click !== null) {
                     this.eventCallback.click();
                 }
             } else {
