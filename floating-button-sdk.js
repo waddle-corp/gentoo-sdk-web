a1bb193f33e80db29ec7bab946bafd7c79d9abfa
eunji9128 <eunji9128@gmail.com>
2025-06-08 10:47:14 -0700

    test: chatUserId logic test for jgd

--- floating-button-sdk.js
+++ floating-button-sdk.js
@@ -110,7 +110,7 @@ class FloatingButton {
             this.fetchChatUserId(this.authCode, this.udid).then((res) => {
                 if (!res) throw new Error("Failed to fetch chat user ID");
                 this.chatUserId = res;
-                this.gentooSessionData.cuid = res;
+                this.gentooSessionData.cuid = this.authCode.includes('guest') ? '' : res;
                 sessionStorage.setItem('gentoo', JSON.stringify(this.gentooSessionData));
             })
             .catch(() => {
