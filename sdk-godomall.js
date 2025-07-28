fee017ac58f3ebf662db4f1897f6ef7e67df9ef5
Ryu Ihchae <ihchaeryu@kaist.ac.kr>
2025-07-28 20:13:53 +0900

    feat: sdk godomall file init

---
+++ sdk-godomall.js
@@ -0,0 +1,46 @@
+(
+    function (global, document) {
+        var w = global;
+        if (w.GentooIO) { 
+            return w.console.error("GentooIO script included twice"); 
+        }; 
+        var ge = function () { 
+            ge.c(arguments); 
+        }; 
+        ge.q = []; 
+        ge.c = function (args) { 
+            ge.q.push(args) 
+        }; 
+        w.GentooIO = ge; 
+        function l() { 
+            if (w.GentooIOInitialized) { return }; 
+            w.GentooIOInitialized = true; 
+            var s = document.createElement("script"); 
+            s.type = "text/javascript"; 
+            s.async = true; 
+            // s.src = "https://sdk.gentooai.com/floating-button-sdk-godomall.js"; 
+            // s.src = 'https://dev-sdk.gentooai.com/floating-button-sdk-godomall.js'; // dev
+            // s.src = "./floating-button-sdk-godomall.js"; 
+            s.src = "https://dev-sdk.gentooai.com/dist/godomall/floating-button-sdk-godomall.js";
+            s.onload = () => { 
+                w.addEventListener("message", ()=>{})
+            }; 
+            var x = document.getElementsByTagName("script")[0]; 
+            if (x.parentNode) { 
+                x.parentNode.insertBefore(s, x) 
+            }; 
+        }; 
+        if (document.readyState === "complete") { 
+            l(); 
+        } else { 
+            w.addEventListener("DOMContentLoaded", l); 
+            w.addEventListener("load", l); 
+        };
+    }
+)(window, document);
+
+GentooIO('boot', {
+    partnerType: 'godomall',
+})
+
+GentooIO('init', {});
