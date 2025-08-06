db220f24fddc1db720dbb0ecdd9ae2e23508d6eb
eunji9128 <eunji9128@gmail.com>
2025-08-06 10:53:24 +0900

    fix: dev domain

--- sdk-godomall.js
+++ sdk-godomall.js
@@ -21,8 +21,8 @@
             // s.src = "https://sdk.gentooai.com/floating-button-sdk-godomall.js"; 
             // s.src = 'https://dev-sdk.gentooai.com/floating-button-sdk-godomall.js'; // dev
             // s.src = "./floating-button-sdk-godomall.js"; 
-            s.src = "https://sdk.gentooai.com/dist/godomall/floating-sdk-godomall.js";
-            // s.src = "https://dev-sdk.gentooai.com/dist/godomall/floating-sdk-godomall.js";
+            // s.src = "https://sdk.gentooai.com/dist/godomall/floating-sdk-godomall.js";
+            s.src = "https://dev-sdk.gentooai.com/dist/godomall/floating-sdk-godomall.js";
             s.onload = () => { 
                 w.addEventListener("message", ()=>{})
             }; 
