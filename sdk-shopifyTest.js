d9a393da74b87dd5d69f51e5098e78fceeda879f
Jintae Song(Tao) <w.jinttao@gmail.com>
2025-08-14 13:18:25 -0700

    add shopify floating test draft

---
+++ sdk-shopifyTest.js
@@ -0,0 +1,40 @@
+(
+    function (global, document) {
+        var w = global;
+        if (w.GentooIO) { 
+            return w.console.error("GentooIO script included twice"); 
+        }; 
+        var ge = function () { 
+            ge.c(arguments); 
+        }; 
+        ge.q = []; 
+        ge.c = function (args) { 
+            ge.q.push(args) 
+        }; 
+        w.GentooIO = ge; 
+        function l() { 
+            if (w.GentooIOInitialized) { return }; 
+            w.GentooIOInitialized = true; 
+            var s = document.createElement("script"); 
+            s.type = "text/javascript"; 
+            s.async = true; 
+            // 🧪 Shopify 테스트용 SDK 로드
+            s.src = "https://sdk.gentooai.com/floating-button-sdk-shopifyTest.js"; 
+            // s.src = 'https://dev-sdk.gentooai.com/floating-button-sdk-shopifyTest.js'; // dev
+            // s.src = "./floating-button-sdk-shopifyTest.js"; 
+            s.onload = () => { 
+                w.addEventListener("message", ()=>{})
+            }; 
+            var x = document.getElementsByTagName("script")[0]; 
+            if (x.parentNode) { 
+                x.parentNode.insertBefore(s, x) 
+            }; 
+        }; 
+        if (document.readyState === "complete") { 
+            l(); 
+        } else { 
+            w.addEventListener("DOMContentLoaded", l); 
+            w.addEventListener("load", l); 
+        };
+    }
+)(window, document);
