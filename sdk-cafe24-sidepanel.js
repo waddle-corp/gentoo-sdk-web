5e87249a7beddca6913ec602940d2d5122d8c0ad
Ryu Ihchae <ihchaeryu@kaist.ac.kr>
2025-09-01 18:03:23 +0900

    feat: sidepanel testing sdk
    '

---
+++ sdk-cafe24-sidepanel.js
@@ -0,0 +1,45 @@
+(
+    function (global, document) {
+        var w = global;
+        if (w.GentooIO) { 
+            return w.console.error("GentooIO script included twice"); 
+        }; 
+        var ge = function () { 
+            ge.c(arguments); 
+        }; 
+        ge.q = []; 
+        ge.c = function (args) { 
+            ge.q.push(args) 
+        }; 
+        w.GentooIO = ge; 
+        function l() { 
+            if (w.GentooIOInitialized) { return }; 
+            w.GentooIOInitialized = true; 
+            var s = document.createElement("script"); 
+            s.type = "text/javascript"; 
+            s.async = true; 
+            // s.src = "https://sdk.gentooai.com/dist/cafe24/floating-cafe24-glacier.js";
+            s.src = "https://dev-sdk.gentooai.com/dist/cafe24/floating-cafe24-sidepanel.js";
+            // s.src = "./dist/cafe24/floating-cafe24-glacier.js"; 
+            s.onload = () => { 
+                w.addEventListener("message", ()=>{})
+            }; 
+            var x = document.getElementsByTagName("script")[0]; 
+            if (x.parentNode) { 
+                x.parentNode.insertBefore(s, x) 
+            }; 
+        }; 
+        if (document.readyState === "complete") { 
+            l(); 
+        } else { 
+            w.addEventListener("DOMContentLoaded", l); 
+            w.addEventListener("load", l); 
+        };
+    }
+)(window, document);
+
+GentooIO('boot', {
+    partnerType: 'cafe24',
+})
+
+GentooIO('init', {});
